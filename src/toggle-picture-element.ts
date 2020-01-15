52d32411bdd00da32de788f8594cfab1b13c47d5
Duy Nguyen <dooz127@gmail.com>
2020-01-15 00:32:37 -0800

    Initial commit

---
+++ src/toggle-picture-element.ts
@@ -0,0 +1,55 @@
+import { LitElement, html, customElement, property, TemplateResult } from 'lit-element';
+import { HomeAssistant } from 'custom-card-helpers';
+import { TogglePictureElementConfig } from './types';
+import { VERSION } from './const';
+import { localize } from './localize/localize';
+
+/* eslint no-console: 0 */
+console.info(
+  `%c  TOGGLE-PICTURE-ELEMENT \n%c  ${localize('common.version')} ${VERSION}    `,
+  'color: orange; font-weight: bold; background: black',
+  'color: white; font-weight: bold; background: dimgray',
+);
+
+@customElement('toggle-picture-element')
+export class TogglePictureElement extends LitElement {
+  @property() public hass?: HomeAssistant;
+  @property() private _config?: TogglePictureElementConfig;
+
+  public setConfig(config: TogglePictureElementConfig): void {
+    if (!config) {
+      throw new Error(localize('common.invalid_configuration'));
+    }
+
+    if (!config.entity) {
+      // TODO: localize
+      throw new Error('Invalid configuration: No entity defined');
+    }
+
+    this._config = config;
+  }
+
+  protected render(): TemplateResult | void {
+    if (!this._config || !this.hass) {
+      return html``;
+    }
+
+    const _entity = this.hass.states[this._config.entity];
+
+    const name =
+      this._config.name !== undefined
+        ? this._config.name
+        : _entity
+        ? _entity.attributes.friendly_name || _entity.entity_id
+        : undefined;
+
+    return html`
+      <ha-entity-toggle
+        .hass=${this.hass}
+        .stateObj=${_entity}
+      ></ha-entity-toggle>
+        <div>${name}</div>
+      </div>
+    `;
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,5 @@
-import { ActionConfig } from 'custom-card-helpers';
-
-// TODO Add your configuration elements here for type-checking
-export interface BoilerplateCardConfig {
+export interface TogglePictureElementConfig {
   type: string;
+  entity: string;
   name?: string;
-  show_warning?: boolean;
-  show_error?: boolean;
-  test_gui?: boolean;
-  entity?: string;
-  tap_action?: ActionConfig;
-  hold_action?: ActionConfig;
-  double_tap_action?: ActionConfig;
 }
